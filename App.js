f79ff63934210c7beac316570886e4694d54a804
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-27 23:16:43 -0500

    Example Component API

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-export default class App extends React.Component {
+class Counter extends Component {
+
+  state = {count: 0}
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000)
+  }
+
+  render() {
+    const {count} = this.state
+    const {color, size} = this.props
+
+    return (
+      <Text style={{color, fontSize: size}}>
+        {count}
+      </Text>
+    )
+  }
+}
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Aprender React Native me ayudara en mi camino como React Developer</Text>
+        <Counter color={'lightblue'} size={16} />
+        <Counter color={'skyblue'} size={32} />
+        <Counter color={'steelblue'} size={80} />
+        <Counter color={'darkblue'} size={140} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+})
---
+++ EXAMPLE.md
@@ -0,0 +1,3 @@
+# Example Component API
+
+The following example includes a `Counter` component that maintains the elapsed time internally as `state.count`. The `App` component renders the `Counter` component with two `props`: `size` and `color`. The App can easily render several Counter components with different sizes and colors.
