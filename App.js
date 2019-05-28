8ba58651bb486d85c07d5c9cd6f66b944629e868
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-27 23:46:39 -0500

    Example core component Text

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Aprender React Native me ayudara en mi camino como React Developer</Text>
+        <Text style={styles.text}>Hello!</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  text: {
+    backgroundColor: 'whitesmoke',
+    color: '#4A90E2',
+    fontSize: 24,
+    padding: 10,
+  },
+})
---
+++ EXAMPLE.md
@@ -0,0 +1,5 @@
+# Example Core Component - Text
+
+A React component for displaying text.
+
+`Text` supports nesting, styling, and touch handling.
