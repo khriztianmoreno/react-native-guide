23931fc1ef3a010fdf8035d44baf60ab9dd6f7b2
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-28 00:35:36 -0500

    Example core component FlatList

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
+
+const rows = [
+  { id: 0, text: 'View' },
+  { id: 1, text: 'Text' },
+  { id: 2, text: 'Image' },
+  { id: 3, text: 'ScrollView' },
+  { id: 4, text: 'ListView' },
+]
+
+const extractKey = ({ id }) => id.toString()
 
 export default class App extends React.Component {
+
+  renderItem = ({ item }) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Aprender React Native me ayudara en mi camino como React Developer</Text>
-      </View>
+      <FlatList
+        style={styles.container}
+        data={rows}
+        renderItem={this.renderItem}
+        keyExtractor={extractKey}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
+  },
+})
+
