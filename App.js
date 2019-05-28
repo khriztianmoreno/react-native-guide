f2b01d10087e8144c76a7d31c5e9304fce076ad6
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-28 00:40:28 -0500

    Example core component SectionList

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { SectionList, Text, StyleSheet } from 'react-native';
 
-export default class App extends React.Component {
+const sections = [
+  {
+    id: 0,
+    title: 'Basic Components',
+    data: [
+      {id: 0, text: 'View'},
+      {id: 1, text: 'Text'},
+      {id: 2, text: 'Image'},
+    ]
+  },
+  {
+    id: 1,
+    title: 'List Components',
+    data: [
+      {id: 3, text: 'ScrollView'},
+      {id: 4, text: 'ListView'},
+    ]
+  }
+]
+
+const extractKey = ({id}) => id.toString()
+
+export default class App extends Component {
+
+  renderItem = ({item}) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
+  renderSectionHeader = ({section}) => {
+    return (
+      <Text style={styles.header}>
+        {section.title}
+      </Text>
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Aprender React Native me ayudara en mi camino como React Developer</Text>
-      </View>
+      <SectionList
+        style={styles.container}
+        sections={sections}
+        renderItem={this.renderItem}
+        renderSectionHeader={this.renderSectionHeader}
+        keyExtractor={extractKey}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
+  },
+  header: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'steelblue',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+})
