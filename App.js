d39a8a7e28ac184b5462962d8345e26312082dad
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-29 00:14:05 -0500

    Example ui libraries - Native base

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  Accordion,
+  Form, Item, Input, Label,
+} from 'native-base';
+
+const dataArray = [
+  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
 
 export default class App extends React.Component {
+  /**
+   * NativeBase use some custom fonts that can be loaded using
+   * Font.loadAsync function.
+   */
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Aprender React Native me ayudara en mi camino como React Developer</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <Text>
+            This is Content Section
+          </Text>
+          <Accordion dataArray={dataArray} expanded={0}/>
+        </Content>
+        <Content>
+          <Form>
+            <Item fixedLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item fixedLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
