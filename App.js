748c07461b69173c1900d693c5fc06b9cb2545b9
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-29 13:30:05 -0500

    Example ReactNative API - webview

--- App.js
+++ App.js
@@ -1,60 +1,20 @@
-import React, { Component } from 'react'
-import { View, Text, AsyncStorage, StyleSheet } from 'react-native'
-
-import Input from './Input'
-
-const STORAGE_KEY = 'ASYNC_STORAGE_NAME_EXAMPLE'
-
-export default class App extends Component {
-
-  state = {name: 'World'}
-
-  componentWillMount() {
-    this.load()
-  }
-
-  load = async () => {
-    try {
-      const name = await AsyncStorage.getItem(STORAGE_KEY)
-
-      if (name !== null) {
-        this.setState({name})
-      }
-    } catch (e) {
-      console.error('Failed to load name.')
-    }
-  }
-
-  save = async (name) => {
-    try {
-      await AsyncStorage.setItem(STORAGE_KEY, name)
-
-      this.setState({name})
-    } catch (e) {
-      console.error('Failed to save name.')
-    }
-  }
-
-  render() {
-    const {name} = this.state
-
-    return (
-      <View>
-        <Input
-          placeholder={'Type your name, hit enter, and refresh!'}
-          onSubmitEditing={this.save}
-        />
-        <Text style={styles.text}>
-          Hello {name}!
-        </Text>
-      </View>
-    )
-  }
-}
+import React from 'react'
+import { View, WebView, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-  text: {
-    padding: 15,
-    backgroundColor: 'skyblue',
-  },
+  container: {
+    flex: 1,
+  }
 })
+
+const WebViewExample = () => {
+   return (
+    <View style = {styles.container}>
+      <WebView
+        source = {{ uri: 'https://khriztianmoreno.com' }}
+      />
+    </View>
+   )
+}
+
+export default WebViewExample;
