57641d1b81c17a7cff1f90091bd8e7fe05dd8ed1
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-29 12:06:02 -0500

    Example data management - redux

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native'
+import { createStore } from 'redux'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Aprender React Native me ayudara en mi camino como React Developer</Text>
-      </View>
-    );
-  }
+// Define action types
+const types = {
+  INCREMENT: 'INCREMENT',
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Define a reducer
+const reducer = (state, action) => {
+  if (action.type === types.INCREMENT) {
+    return {count: state.count + 1}
+  }
+  return state
+}
+
+// Define the initial state of our store
+const initialState = {count: 0}
+
+// Create a store, passing our reducer function and our initial state
+const store = createStore(reducer, initialState)
+
+
+/// We're done! Redux is all set up. Here's how we can use it:
+
+
+// Now we can dispatch actions, which are understood by our store/reducer
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+
+// Calling `store.getState()` returns our state object
+const App = () => (
+  <Text style={{fontSize: 100}}>
+    {store.getState().count}
+  </Text>
+)
