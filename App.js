5bf2d7d5ea82d621f131a47ad1efddf733b6810d
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-28 23:37:05 -0500

    Example flex dimensions

--- App.js
+++ App.js
@@ -1,65 +1,17 @@
-import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-import Toggle from './Toggle'
-
-export default class App extends Component {
-
-  state = {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-
+export default class FlexDimensionsBasics extends Component {
   render() {
-    const {flexDirection, alignItems, justifyContent} = this.state
-    const layoutStyle = {flexDirection, justifyContent, alignItems}
-
-    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
-
     return (
-      <View style={styles.container}>
-        <Toggle
-          label={'Primary axis (flexDirection)'}
-          value={flexDirection}
-          options={['row', 'column']}
-          onChange={(option) => this.setState({flexDirection: option})}
-        />
-        <Toggle
-          label={primaryAxis + ' distribution (justifyContent)'}
-          value={justifyContent}
-          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
-          onChange={(option) => this.setState({justifyContent: option})}
-        />
-        <Toggle
-          label={secondaryAxis + ' alignment (alignItems)'}
-          value={alignItems}
-          options={['flex-start', 'center', 'flex-end', 'stretch']}
-          onChange={(option) => this.setState({alignItems: option})}
-        />
-        <View style={[styles.layout, layoutStyle]}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </View>
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
-    )
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-  },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: 'steelblue',
-    margin: 5,
-  },
-})
