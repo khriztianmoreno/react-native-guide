19136b5fcb67620095016508120fddc98878025a
khriztianmoreno <khriztianmoreno@gmail.com>
2019-05-30 12:38:01 -0500

    Exercise profile

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, ThemeProvider, Avatar, Input } from 'react-native-elements';
+import { View, Text, Image, StyleSheet } from 'react-native'
+import { Avatar, Card, ListItem, Button, Icon } from 'react-native-elements'
+
+const users = [
+  {
+     name: 'brynn',
+     avatar: 'https://khriztianmoreno.com/img/profile.jpg'
+  },
+ ];
 
 export default class App extends React.Component {
   render() {
     return (
-      <ThemeProvider>
-        <Button title="Hey!" style={styles.button} />
-        <Avatar
-          rounded
-          source={{
-            uri:
-              'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-          }}
-        />
-        <Input
-          placeholder='INPUT WITH ERROR MESSAGE'
-          errorStyle={{ color: 'red' }}
-          errorMessage='ENTER A VALID ERROR HERE'
-        />
-      </ThemeProvider>
+      <View style={styles.container}>
+        <View style={{paddingTop: 40}}>
+          <Card
+            title='HELLO WORLD'
+            image={{uri: 'https://pbs.twimg.com/profile_banners/57435440/1520374386/1500x500'}}>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <View style={{height: 100}}>
+                <Avatar
+                  rounded
+                  size="large"
+                  source={{
+                    uri:
+                      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                  }}
+                />
+              </View>
+            <Button
+              icon={<Icon name='code' color='#ffffff' />}
+              backgroundColor='#03A9F4'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='VIEW NOW' />
+          </Card>
+        </View>
+      </View>
     );
   }
 }
